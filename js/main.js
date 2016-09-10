2086924d1bbfaa0767df26b05951c301fed06709
muit <miguel_3c@hotmail.com>
2016-09-10 21:49:59 +0200

    Created PressKit ( english & spanish)

--- js/main.js
+++ js/main.js
@@ -33,4 +33,72 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
+  if (typeof(Storage) !== "undefined") {
+    Lenguaje.saveDefaultOrSaved();
+
+    $('.lenguaje-switch').click(function (e) {
+      e.preventDefault();
+      Lenguaje.setLenguaje(Lenguaje.isEnglish()? "spanish" : "english");
+    })
+  } else {
+    console.log("No Web Storage support..");
+    Lenguaje.setLenguaje("english");
+  }
+
 })
+
+var Lenguaje = {
+
+  saveDefaultOrSaved() {
+    var lenguajeParam = getParameterByName("ln");
+    if(lenguajeParam) {
+      if(lenguajeParam == "en") {
+        this.setLenguaje("english");
+        return true;
+      }
+      if(lenguajeParam == "es")  {
+        this.setLenguaje("spanish");
+        return true;
+      }
+    }
+
+    if(!localStorage.lenguaje) {
+      this.setLenguaje("english");
+      return true;
+    }
+    this.setLenguaje(localStorage.lenguaje);
+   return false;
+  },
+
+  isEnglish() {
+    return localStorage.lenguaje == "english"
+  },
+
+  setLenguaje(name) {
+    if(typeof(Storage) !== "undefined")
+      localStorage.lenguaje = name;
+
+    $('.lenguaje').removeClass('visible');
+    console.log('.lenguaje.'+name);
+    $('.lenguaje.'+name).addClass('visible');
+    this.updateName();
+  },
+
+  updateName () {
+    if(typeof(Storage) !== "undefined") {
+      $('.lenguaje-switch').html(this.isEnglish()? "Español" : "English");
+      return;
+    }
+    $('.lenguaje-switch').html("Español");
+  }
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
