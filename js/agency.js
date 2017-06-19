4036a564b9cea841b8c9ce6479bdab7fda1851e7
muit <muit@piperift.com>
2017-06-19 13:46:12 +0200

    Updated Bootstrap, Added Posts

--- js/agency.js
+++ js/agency.js
@@ -17,7 +17,7 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -11,7 +11,7 @@
 var cbpAnimatedHeader = (function() {
 
 	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-fixed-top' ),
+		header = document.querySelector( '.fixed-top' ),
 		didScroll = false,
 		changeHeaderOn = 200;
 
