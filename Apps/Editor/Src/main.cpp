6f8e530738fa6cbd9f20b4b8830225c4f28b999b
muit <muit@piperift.com>
2021-02-18 15:41:52 +0100

    Running editor as win32

--- Apps/Editor/Src/main.cpp
+++ Apps/Editor/Src/main.cpp
@@ -4,9 +4,20 @@
 
 #include <Context.h>
 
+#if PLATFORM_WINDOWS
+#	pragma comment(linker, "/subsystem:windows")
+#	include <windows.h>
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+	Rift::Context::Initialize();
+	Window window{};
+	return window.Run();
+}
+#else
 int main(int, char**)
 {
 	Rift::Context::Initialize();
 	Window window{};
 	return window.Run();
 }
+#endif
