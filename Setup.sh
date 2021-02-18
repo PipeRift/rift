addec70db98ed0927536a12d3766e48e20a21786
muit <muit@piperift.com>
2021-02-18 09:48:29 +0100

    tests will now build again

--- Setup.sh
+++ Setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-apt-get -y update
-apt-get -y upgrade
-
-apt-get -y install build-essential xz-utils curl libx11-dev
+sudo apt-get install xorg-dev libglu1-mesa-dev
+apt-get -y install build-essential xz-utils curl libx11-dev xorg-dev libglu1-mesa-dev
 apt-get -y install llvm clang clang-tidy clang-format
 
 apt-get -y install gdb
 
 apt-get -y install cmake
-
-apt-get -y install python3 python3-pip
-pip3 install conan
-
-ln -s ./Build/compile_commands.json ./compile_commands.json
