35d6558ec7c6a71f23d2612a3afd3d9cae41a099
Tharrik <tharrik@hotmail.com>
2021-02-22 19:14:38 +0100

    Yet another minor fix (FFS)

--- Apps/Editor/Include/Editors/Projects/FileExplorerPanel.h
+++ Apps/Editor/Include/Editors/Projects/FileExplorerPanel.h
@@ -1,6 +1,13 @@
 // Copyright 2015-2021 Piperift - All rights reserved
 #pragma once
 
+#include "Strings/String.h"
+#include "Assets/TypeAsset.h"
+#include "Assets/AssetPtr.h"
+#include "Containers/Array.h"
+
+using namespace Rift;
+
 // Forward declarations
 class ProjectEditor;
 
