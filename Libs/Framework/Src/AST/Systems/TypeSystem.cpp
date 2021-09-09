82b8dedbd03569ca7461a5ee367293270a979174
muit <muit@piperift.com>
2021-09-09 22:25:03 +0200

    Cache existing types in the AST

--- Libs/Framework/Src/AST/Systems/TypeSystem.cpp
+++ Libs/Framework/Src/AST/Systems/TypeSystem.cpp
@@ -10,7 +10,7 @@ namespace Rift::TypeSystem
 {
 	void OnNewType(AST::Tree::Registry& registry, AST::Id typeId)
 	{
-		CType& type    = registry.get<CType>(typeId);
+		const CType& type = registry.get<const CType>(typeId);
 		auto& typeList = registry.ctx<CTypeListUnique>();
 		typeList.types.Insert(type.id, typeId);
 	}
@@ -26,6 +26,15 @@ namespace Rift::TypeSystem
 	{
 		auto& typeList = ast.GetOrSetUnique<CTypeListUnique>();
 		typeList.types.Empty();
+
+		// Cache existing types
+		auto typesView = ast.MakeView<const CType>();
+		typeList.types.Reserve(typesView.Size());
+		for (AST::Id typeId : typesView)
+		{
+			const CType& type = typesView.Get<const CType>(typeId);
+			typeList.types.Insert(type.id, typeId);
+		}
 
 		ast.OnConstruct<CType>().connect<&OnNewType>();
 		ast.OnDestroy<CType>().connect<&OnDeleteType>();
