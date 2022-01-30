351f12f75be0815f7439153a52d4b56035ef9ed1
muit <muit@piperift.com>
2022-01-30 14:30:28 +0100

    [CICD] LLVM fixes

--- Libs/Backends/LLVM/Src/LLVMBackend.cpp
+++ Libs/Backends/LLVM/Src/LLVMBackend.cpp
@@ -2,16 +2,16 @@
 
 #include "LLVMBackend.h"
 
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LLVMContext.h>
-#include <llvm/IR/Module.h>
+//#include <llvm/IR/IRBuilder.h>
+//#include <llvm/IR/LLVMContext.h>
+//#include <llvm/IR/Module.h>
 
 
 namespace Rift::Compiler
 {
 	void LLVMBackend::Build(Context& context) override
 	{
-		llvm::IRBuilder<> builder(TheContext);
+		// llvm::IRBuilder<> builder(TheContext);
 		context.AddError("LLVM backend is not yet supported.");
 	}
 }    // namespace Rift::Compiler
