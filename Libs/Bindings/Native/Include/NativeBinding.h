3b11fd1466af169d22942a92583504580ec8b79a
muit <muit@piperift.com>
2022-08-27 12:32:22 +0200

    Added Clang

---
+++ Libs/Bindings/Native/Include/NativeBinding.h
@@ -0,0 +1,18 @@
+// Copyright 2015-2022 Piperift - All rights reserved
+
+#pragma once
+
+#include <Plugin.h>
+#include <Rift.h>
+
+
+namespace rift
+{
+	class NativeBindingPlugin : public Plugin
+	{
+		CLASS(NativeBindingPlugin, Plugin)
+
+	public:
+		void Register(TPtr<Rift> rift) override {}
+	};
+}    // namespace rift
